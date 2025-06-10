63ac0a49655326a5bdf999b8ebd3655a72b5847e
AbelMaireg <abelrighthere@gmail.com>
2025-06-10 18:05:49 +0300

    refactor: using map instead of index counter.

--- app.py
+++ app.py
@@ -7,6 +7,71 @@ from PIL import Image
 # Load the trained model
 model = tf.keras.models.load_model("./model/emnist_model.keras")
 
+maps = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F",
+    16: "G",
+    17: "H",
+    18: "I",
+    19: "J",
+    20: "K",
+    21: "L",
+    22: "M",
+    23: "N",
+    24: "O",
+    25: "P",
+    26: "Q",
+    27: "R",
+    28: "S",
+    29: "T",
+    30: "U",
+    31: "V",
+    32: "W",
+    33: "X",
+    34: "Y",
+    35: "Z",
+    36: "a",
+    37: "b",
+    38: "c",
+    38: "d",
+    39: "e",
+    40: "f",
+    41: "g",
+    42: "h",
+    43: "i",
+    44: "j",
+    45: "k",
+    46: "l",
+    47: "m",
+    48: "n",
+    49: "o",
+    50: "p",
+    51: "q",
+    52: "r",
+    53: "s",
+    54: "t",
+    55: "u",
+    56: "v",
+    57: "w",
+    58: "x",
+    59: "y",
+    60: "z",
+}
+
 
 def preprocess_image(file_stream):
     """Preprocess image for MNIST digit prediction"""
@@ -43,7 +108,7 @@ def create_app():
             prediction = model.predict(img_array)
             predicted_index = int(np.argmax(prediction[0]))
             confidence = float(prediction[0][predicted_index])
-            predicted_character = predicted_index
+            predicted_character = maps[predicted_index]
             if predicted_index < 10:
                 predicted_character = predicted_index
             else:
